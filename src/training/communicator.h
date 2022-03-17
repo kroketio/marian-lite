a4374ac953d2a2891904d17cb287522d390512ef
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-03-17 15:30:43 +0000

    Remove unused variable

--- src/training/communicator.h
+++ src/training/communicator.h
@@ -102,7 +102,6 @@ private:
       int totalSize = (int)graphs_[0]->params()->vals()->size();
       int shardSize = (int)ceil(totalSize / (float)graphs_.size());
 
-      int pos = 0;
       for(auto graph : graphs_) {
         int __size__ = std::min(shardSize, totalSize);
 
@@ -117,7 +116,6 @@ private:
         tmpTensors_.push_back(tmp);
 
         // move to next shard
-        pos += __size__;
         totalSize -= __size__;
       }
     }
