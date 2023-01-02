01a2cf5f37654be76fd8c6bf6ac394601f9c16a0
Sander <code@kroket.io>
2023-01-02 02:50:02 +0200

    pin vendored libs by tag

--- cmake/DownloadAllTheThings.cmake
+++ cmake/DownloadAllTheThings.cmake
@@ -6,7 +6,7 @@ message(STATUS "Vendoring libs for marian-lite")
 
 FetchContent_Declare(intgemm
     GIT_REPOSITORY https://github.com/kroketio/intgemm.git
-    GIT_TAG "b5da8f1d3d79f90a85332a293c69004d62e0406c"
+    GIT_TAG "0.0.3"
     GIT_SHALLOW    TRUE
     GIT_PROGRESS   TRUE
 )
@@ -14,7 +14,7 @@ FetchContent_MakeAvailable(intgemm)
 
 FetchContent_Declare(pathie
         GIT_REPOSITORY https://github.com/kroketio/pathie-cpp.git
-        GIT_TAG "5a9732f5a8e2c3c1508a7aef526ae4c001c4f262"
+        GIT_TAG "0.1.3"
         GIT_SHALLOW    TRUE
         GIT_PROGRESS   TRUE
         )
@@ -23,7 +23,7 @@ FetchContent_GetProperties(pathie)
 
 FetchContent_Declare(sentencepiece-browsermt
         GIT_REPOSITORY https://github.com/kroketio/sentencepiece-browsermt.git
-        GIT_TAG "539cbc91704886c539286806eb2883f927000839"
+        GIT_TAG "0.1.96"
         GIT_SHALLOW    TRUE
         GIT_PROGRESS   TRUE
         )
